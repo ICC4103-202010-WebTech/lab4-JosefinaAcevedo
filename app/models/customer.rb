4342e55141d6062efb36cbc6408356b6cca84ab5
JosefinaAcevedo <jacevedo@miuandes.cl>
2020-04-11 12:16:58 -0400

    add changes

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ApplicationRecord
   has_many :orders
   has_many :tickets, through: :orders
+
+  validates :name, :lastname, :email, presence:true
+  validates :email, format:{with:/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+  validates :email, uniqueness:true
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,12 +2,27 @@ class Ticket < ApplicationRecord
   belongs_to :order
   belongs_to :ticket_type
 
-  # TODO: complete the folowing
-  # before_xxxxx :update_stats
+  # TODO: complete the following
+  before_create :update_stats
+  before_destroy :update_stats1
 
   private
     def update_stats
       es = self.ticket_type.event.event_stat
+      er = self.ticket_type.event.event_venue
       # TODO: complete in order to update event stats
+      if es.tickets_sold +1 > er.capacity
+        raise "All the tickets are sold!"
+      else
+        es.tickets_sold = es.tickets_sold +1
+        es.attendance = es.attendance +1
+      end
     end
+
+    def update_stats1
+      ed = self.ticket_type.event.event_stat
+      ed.tickets_sold = ed.tickets_sold -1
+      ed.attendance = ed.attendance -1
+    end
+
 end
